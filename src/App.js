5a010393c00a6f78baf1473a607c9b9d1da2a580
Brassabrod <emerson.suckow@gmail.com>
2025-07-14 11:27:40 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./style.css";
 import "./index.css";
 
-import Header from "./components/Header";
-import Footer from "./Footer";
-
-import Calculadora from "./components/Calculadora";
+import Header          from "./components/Header";
+import Footer          from "./components/Footer";
+import Calculadora     from "./components/Calculadora";
 import BreadCalculator from "./components/BreadCalculator";
-import PremiumGate from "./components/PremiumGate";
-import Login from "./components/Login";
+import PremiumGate     from "./components/PremiumGate";
+import Login           from "./components/Login";
 
 export default function App() {
   return (
@@ -18,14 +18,14 @@ export default function App() {
       {/* cabeçalho fixo em todas as páginas */}
       <Header />
 
-      {/* somente <Route> dentro de <Routes> */}
+      {/* conteúdo dinâmico por rota */}
       <Routes>
         <Route path="/" element={<Calculadora />} />
         <Route path="/custos" element={<BreadCalculator />} />
         <Route path="/premium" element={<PremiumGate />} />
         <Route path="/login" element={<Login />} />
-        {/* se quiser tratar 404, pode adicionar:
-            <Route path="*" element={<NotFound />} /> 
+        {/* Para rota 404, se quiser:
+            <Route path="*" element={<NotFound />} />
         */}
       </Routes>
 
