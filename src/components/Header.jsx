7ae24edbf5e7981197ec75cf676370b4c5ffd5fb
Brassabrod <emerson.suckow@gmail.com>
2025-07-14 15:20:04 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,23 +6,15 @@ export default function Header() {
   return (
     <header className="bg-purple-700 text-white shadow px-6 py-4 flex justify-between items-center">
       <Link to="/">
--       <img
--         src={`${process.env.PUBLIC_URL}/logo.png`}
--         alt="Pão 3x4"
--         className="h-12"
--       />
-+       <img
-+         src={`${process.env.PUBLIC_URL}/logo.png`}
-+         alt="Pão 3x4"
-+         className="h-12"
-+       />
+        <img
+          src={`${process.env.PUBLIC_URL}/logo_pao3x4.png`}
+          alt="Pão 3×4"
+          className="h-12"
+        />
       </Link>
       <nav className="space-x-6">
         <Link to="/" className="hover:underline">Massa</Link>
         <Link to="/custos" className="hover:underline">Custo Pães</Link>
-         <Link to="/premium" className="hover:underline">Premium</Link>
-        <Link to="/login" className="hover:underline">Login</Link>
-       
       </nav>
     </header>
   );
