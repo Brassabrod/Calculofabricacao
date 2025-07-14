ac69f1d61640fa934d0e47cf59fd015bee24dd4d
Brassabrod <emerson.suckow@gmail.com>
2025-07-14 15:26:58 +0200

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
     <header className="bg-purple-700 text-white shadow px-6 py-4 flex justify-between items-center">
       <Link to="/">
         <img
-          src={`${process.env.PUBLIC_URL}/logo.png`}
+          src={`${process.env.PUBLIC_URL}/logo_pao3x4.png`}
           alt="Pão 3×4"
           className="h-12"
         />
